cc15ed2d260473c4b22c1a9c2f2b25b0c42ed58d
Vale-riad <valentinariad91@gmail.com>
2022-11-02 17:33:38 +0100

    Aggiunge due livelli

--- js/main.js
+++ js/main.js
@@ -3,16 +3,52 @@
 const boardContainer = document.querySelector(".board");
 const btnPlay = document.querySelector(".play");
 
-boardContainer.innerHTML="";
+btnPlay.addEventListener('click', function () {
+    const level = document.getElementById("difficoltà").value;
+    if(level==="hard"){
+    boardContainer.innerHTML="";
 for(let i = 1; i<=100; i ++){
     const boardCell = document.createElement("div");
     boardCell.innerHTML=i;
     boardCell.classList.add("board_number");
     boardCell.addEventListener("click", function(){
         console.log(this.innerHTML);
-        
         boardCell.classList.add("color");
         
     })
     boardContainer.append(boardCell);
 }
+}
+else if(level==="medium"){
+    boardContainer.innerHTML="";
+for(let i = 1; i<=81; i ++){
+    const boardCell = document.createElement("div");
+    boardCell.innerHTML=i;
+    boardCell.classList.add("medium-level");
+    boardCell.addEventListener("click", function(){
+        console.log(this.innerHTML);
+        boardCell.classList.add("color");
+        
+    })
+    boardContainer.append(boardCell);
+}
+}
+else if(level==="easy"){
+    boardContainer.innerHTML="";
+for(let i = 1; i<=49; i ++){
+    const boardCell = document.createElement("div");
+    boardCell.innerHTML=i;
+    boardCell.classList.add("easy-level");
+    boardCell.addEventListener("click", function(){
+        console.log(this.innerHTML);
+        boardCell.classList.add("color");
+        
+    })
+    boardContainer.append(boardCell);
+}
+}
+
+
+
+
+})
